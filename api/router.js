cc59040381437d31313c2e2838e587f690080b1c
Andrew Hao <andrewhao@gmail.com>
2016-02-12 07:38:33 -0800

    Break it out.

--- api/router.js
+++ api/router.js
@@ -3,11 +3,25 @@ import { Router } from 'express';
 const router = new Router();
 
 router.get('/', (req, res, next) => {
+  // Am I up?
   res.send('API up!');
 });
 
 router.get('/ping', (req, res, next) => {
+  // - Hit Strava API
+  // - Query for list of commutes
+  // - Save new commutes (id, streamdata) in db
   res.send('Ping');
+});
+
+router.get('/ingest', (req, res, next) => {
+  // - For commutes without reports
+  // - generate report
+  // - save report
+});
+
+router.get('/send', (req, res, next) => {
+  // - email reports that have not been emailed
 });
 
 export default router;
