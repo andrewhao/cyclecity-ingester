62b6473b36db8b6aca6544afed0a88e8f6d43f89
Andrew Hao <andrewhao@gmail.com>
2016-02-15 23:53:32 -0800

    Zipping it all up.

--- api/router.js
+++ api/router.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import StravaService from '../services/strava';
+import _ from 'lodash';
 
 const strava = new StravaService();
 const router = new Router();
@@ -14,7 +15,23 @@ router.get('/streams', (req, res, next) => {
   // - Query for list of commutes
   // - Save new commutes (id, streamdata) in db
   strava.streams().then((data) => {
-    res.send(data);
+    const timeData = _.find(data, { type: 'time' }).data;
+    const latlngData = _.find(data, { type: 'latlng' }).data;
+    const distanceData = _.find(data, { type: 'distance' }).data;
+    const velocityData = _.find(data, { type: 'velocity_smooth' }).data;
+    console.log(timeData);
+    const zipped = _.zipWith(timeData,
+                             latlngData,
+                             distanceData,
+                             velocityData,
+                             (time, latlng, dist, vel) => {
+                               return { time: time,
+                                 latlng: latlng,
+                                 distance: dist,
+                                 velocity: vel }
+                             })
+
+    res.send(zipped);
   })
   .catch((err) => res.send(err));
 });
